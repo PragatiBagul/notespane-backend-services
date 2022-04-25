002fb2632cd20a85d9575b0c32925ccd718b7ee5
PragatiBagul <bagulpragati15@gmail.com>
2022-04-25 17:53:52 +0530

    Reository and controller changes

---
+++ src/main/java/notespane/notespanebackendservices/model/Edge.java
@@ -0,0 +1,18 @@
+package notespane.notespanebackendservices.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+//
+//@Entity
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Table(name = "edge")
+public class Edge {
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    //@Column(name = "id", nullable = false)
+    private Long id;
+
+}
---
+++ src/main/java/notespane/notespanebackendservices/model/EdgeStack.java
@@ -0,0 +1,5 @@
+package notespane.notespanebackendservices.model;
+
+public class EdgeStack {
+
+}
